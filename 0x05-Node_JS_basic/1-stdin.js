951d53e2ed9d0e8d1bf0afb2e6feb135ae4b3128
tolaa <mgamtaa@gmail.com>
2023-03-11 22:18:00 +0300

    error fixed

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,12 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('readable', () => {
- let input;
-while((input = process.stdin.read()) != null){
-process.stdout.write(`Your name is: ${input}`);
-}
+  // const input;
+  const input = process.stdin.read();
+  while (input != null) {
+    process.stdout.write(`Your name is: ${input}`);
+  }
 });
 
 process.stdin.on('end', () => {
-process.stdout.write("This important software is now closing\n")
+  process.stdout.write('This important software is now closing\n');
 });
