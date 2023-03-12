00228a427c56bf16fd4d41d56683b630d0ef024f
tolaa <mgamtaa@gmail.com>
2023-03-12 16:44:57 +0300

    fix

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,45 +1,49 @@
-const fs = require('fs/promises');
-
-async function countStudents(path){
-
-try {
-  const data = await fs.readFile(path, { encoding: 'utf8' });
-  lines = lineCount(data) - 1
-  const words = data.trim().split('\n');
-  words.shift();
-  var csstudents = "";
-  var sestudents = "";
-  words.forEach(element =>{
-     as = element.trim().split(',');
-     if (as[as.length - 1] == 'CS'){
-        csstudents = csstudents + as[0] + ', ';
-      }
-     else{
-       sestudents = sestudents + as[0] + ', ';
-     }
- });
-     
-  const cs = csstudents.trim().slice(0, -1);
-  const se = sestudents.trim().slice(0, -1)
-  const numbcs = cs.split(',').length;
-  const numbse = se.split(',').length;
-  console.log(`Number of students: ${lines}`);
-  console.log(`Number of students in CS: ${numbcs}. List: ${cs}`);
-  console.log(`Number of students in SWE: ${numbse}. List: ${se}`);
-} catch (err) {
-  console.log('Cannot load the database');
-}
-
-}
+// const fs = require('fs/promises');
+const fs = require('fs');
 
 function lineCount(text) {
-    lines = 0;
-    for( let i = 0, n = text.length;  i < n;  ++i ) {
-        if( text[i] === '\n' ) {
-            ++lines;
-        }
+  let lines = 0;
+  for (let i = 0, n = text.length; i < n; i += 1) {
+    if (text[i] === '\n') {
+      lines += 1;
     }
-    return lines;
+  }
+  return lines;
 }
+
+// async function countStudents(path){
+const countStudents = (path) => {
+  const prom = new Promise((resolve, reject) => {
+    try {
+      const data = fs.readFileSync(path, 'utf8');
+      const lines = lineCount(data) - 1;
+      const words = data.trim().split('\n');
+      words.shift();
+      let csstudents = '';
+      let sestudents = '';
+      words.forEach((element) => {
+        const as = element.trim().split(',');
+        if (as[as.length - 1] === 'CS') {
+          csstudents = `${csstudents + as[0]}, `;
+        } else {
+          sestudents = `${sestudents + as[0]}, `;
+        }
+      });
+
+      const cs = csstudents.trim().slice(0, -1);
+      const se = sestudents.trim().slice(0, -1);
+      const numbcs = cs.split(',').length;
+      const numbse = se.split(',').length;
+      console.log(`Number of students: ${lines}`);
+      console.log(`Number of students in CS: ${numbcs}. List: ${cs}`);
+      console.log(`Number of students in SWE: ${numbse}. List: ${se}`);
+      resolve(true);
+      reject(new Error('something bad happened'));
+    } catch (err) {
+      console.log('Error: Cannot load the database');
+    }
+  });
+  return prom;
+};
 
 module.exports = countStudents;
