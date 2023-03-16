4cbfef7a928bbf3424daef20d8874c4147d0699e
tolaa <mgamtaa@gmail.com>
2023-03-16 22:46:59 +0300

    mocha test

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,32 +1,25 @@
-const assert = require('assert');
-const calculateNumber = require('./0-calcul');
+  const assert = require('assert');
+  const calculateNumber = require('./0-calcul');
 
 describe('testing Suite', () => {
-  it('testcase 1', () => {
-    assert.strictEqual(calculateNumber(2.0, 1), 3);
+  it('round the first argument', () => {
+    assert.equal(calculateNumber(2.0, 1), 3);
+    assert.equal(calculateNumber(1.3, 2), 3);
+    assert.equal(calculateNumber(2.5, 3), 6);
   });
 
-  it('testcase 2', () => {
-    assert.strictEqual(calculateNumber(0, 1), 1);
+  it('round the second argument', () => {
+    assert.equal(calculateNumber(1, 2.0), 3);
+    assert.equal(calculateNumber(2, 1.3), 3);
+    assert.equal(calculateNumber(3, 2.5), 6);
   });
 
-  it('testcase 3', () => {
-    assert.strictEqual(calculateNumber(3.5, 4.7), 9);
-  });
-
-  it('testcase 4', () => {
-    assert.strictEqual(calculateNumber(-2, 1), -1);
-  });
-
-  it('testcase 5', () => {
-    assert.strictEqual(calculateNumber(3.49999, 6.49999), 9);
-  });
-
-  it('testcase 6', () => {
-    assert.strictEqual(calculateNumber(-3.45, -2.61), -6);
-  });
-
-  it('testcase 7', () => {
-    assert.strictEqual(calculateNumber(999.999, 1), 1001);
+  it('return correct number', () => {
+    assert.equal(calculateNumber(2.0, 1.5), 4);
+    assert.equal(calculateNumber(1.3, 2.9), 4);
+    assert.equal(calculateNumber(2.49999, 6.1), 8);
+    assert.equal(calculateNumber(3.7, 2), 6);
+    assert.equal(calculateNumber(1.3, 0), 1);
+    assert.equal(calculateNumber(2, 3), 5);
   });
 });
